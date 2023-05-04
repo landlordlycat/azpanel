8e6b5b98f6ea460f636115c5fefb54e1c49b44f9
azpanel <100949601+azpanel@users.noreply.github.com>
2023-05-04 21:00:50 +0800

    add phpinsights

---
+++ phpinsights.php
@@ -0,0 +1,25 @@
+<?php
+
+declare (strict_types = 1);
+
+return [
+    'preset' => 'default',
+    'exclude' => [
+        "runtime",
+        "config",
+        "route",
+    ],
+    'add' => [
+        //  ExampleMetric::class => [
+        //      ExampleInsight::class,
+        //  ]
+    ],
+    'remove' => [
+        //  ExampleInsight::class,
+    ],
+    'config' => [
+        //  ExampleInsight::class => [
+        //      'key' => 'value',
+        //  ],
+    ],
+];
